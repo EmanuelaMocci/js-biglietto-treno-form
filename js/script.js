f09c1c154326866e7bddce71073c2a7eb67f9fe4
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-30 15:56:49 +0200

    Aggiunta struttura di partenza html

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+// Seleziona i bottoni
+var Accedi = document.getElementById("bottoneAccedi");
+var Annulla = document.getElementById("bottoneAnnulla");
+
+
+var bottone = document.getElementById("bottoneAccedi");
+
+// bottone.addEventListener("click", 
+//     function(){
+        
+//     }
+// );
