d450f2c8530891042e4a8c745ae971eba4e2611d
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-30 16:41:33 +0200

    Aggiunta table html

--- js/script.js
+++ js/script.js
@@ -4,15 +4,9 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
-// Seleziona i bottoni
-var Accedi = document.getElementById("bottoneAccedi");
-var Annulla = document.getElementById("bottoneAnnulla");
 
-
-var bottone = document.getElementById("bottoneAccedi");
-
-// bottone.addEventListener("click", 
-//     function(){
-        
-//     }
-// );
+// 1 - faccio la form con i campi;
+// 2 - devo recuperare i dati immessi dall’utente, come faccio? Vado a leggere la documentazione/cerco su google (possibilmente in inglese: trovo più risultati e sono più affidabili);
+// 3 - Devo manipolare i dati: devo controllare SE l’età è < di 18 o > 65. SE sono in uno di questi due casi, applico lo sconto, ALTRIMENTI non lo applico (i se ed altrimenti sono scritti volontariamente in maiuscolo per darti un suggerimento :occhiolino:)
+// 4 - Ottenuto il risultato lo mostro a video.
+// Spezzati sempre il problema in sottoproblemi e vedrai che andrai più spedita. In ogni caso, ti lascio mezz’ora. Se entro mezz’ora non ce la fai a risolvere o ad impostare bene l’esercizio, ricontattami. Se non sono impegnato con un ticket ci sentiamo su zoom e ti do una mano per proseguire. Se vedi che non ti rispondo entro 10 minuti apri un ticket
