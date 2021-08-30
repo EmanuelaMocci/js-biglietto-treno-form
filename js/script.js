b9b20cc3b887cc73ef8987d6599c018cdc327f0b
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-30 18:09:39 +0200

    Aggiunta numero random carrozza

--- js/script.js
+++ js/script.js
@@ -18,32 +18,27 @@ bottoneGenera.addEventListener("click",
         var km = parseInt(document.getElementById("km").value);
         var eta = document.getElementById("eta").value;
 
+        // Codice carrozza
+        var numeroCarrozza = Math.floor(Math.random() * 10);
         // Calcolo prezzo per km
         var prezzo = (km * 0.21);
 
-        // Applicazione sconto del 20% under 18
-        var sconto20 = (prezzo / 100) * 20;
-        var prezzoFinaleMinorenne = (prezzo - sconto20).toFixed(2);
-      
-        // Applicazione sconto del 40% se over 65
-        var sconto40 = (prezzo / 100) * 40; 
-        var prezzoFinale = (prezzo - sconto40).toFixed(2);
-
+        // Condizione sconto
         if( eta == "minorenne"){
-            document.getElementById('costo').innerHTML = prezzoFinaleMinorenne;
-            console.log(prezzoFinaleMinorenne);
+            var sconto20 = (prezzo / 100) * 20;
+            prezzo = (prezzo - sconto20).toFixed(2);
+        
         } else if ( eta == "over"){
-            document.getElementById('costo').innerHTML = prezzoFinale;
-            console.log(prezzoFinale);
+            var sconto40 = (prezzo / 100) * 40; 
+            prezzo = (prezzo - sconto40).toFixed(2);
         } else {
-            document.getElementById('costo').innerHTML = prezzo;
+            
         }
 
         // Ottenuto il risultato lo mostro a video.
         document.getElementById('nome-utente').innerHTML = nome;
-    
-
-
+        document.getElementById('costo').innerHTML = prezzo;
+        document.getElementById('carrozza').innerHTML = numeroCarrozza;
 
     }
 );
@@ -57,4 +52,4 @@ bottoneGenera.addEventListener("click",
 
 // 3 - Devo manipolare i dati: devo controllare SE l’età è < di 18 o > 65. SE sono in uno di questi due casi, applico lo sconto, ALTRIMENTI non lo applico (i se ed altrimenti sono scritti volontariamente in maiuscolo per darti un suggerimento :occhiolino:)
 // 4 - Ottenuto il risultato lo mostro a video.
-// Spezzati sempre il problema in sottoproblemi e vedrai che andrai più spedita. In ogni caso, ti lascio mezz’ora. Se entro mezz’ora non ce la fai a risolvere o ad impostare bene l’esercizio, ricontattami. Se non sono impegnato con un ticket ci sentiamo su zoom e ti do una mano per proseguire. Se vedi che non ti rispondo entro 10 minuti apri un ticket
+// Spezzati sempre il problema in sottoproblemi.
